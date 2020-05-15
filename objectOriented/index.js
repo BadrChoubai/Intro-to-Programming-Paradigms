4d8dea5482a4f4cfe3c0732734bd74d367cb4b82
Badr Choubai <badrchoubai@outlook.com>
2020-05-14 23:10:48 -0600

    Stub out object oriented code

---
+++ objectOriented/index.js
@@ -0,0 +1,17 @@
+/**
+ * Object Oriented Programming Code
+ *
+ * Let's think about our application in an
+ * object-oriented way.
+ *
+ * What objects could we work with?
+ * - Our form
+ * - The form validation logic
+ * - The User we create in the end
+ */
+
+class FormValidation {}
+
+class User {}
+
+class UserInputForm {}
