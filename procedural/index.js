15e53dbf99bb0a0151ef866a85b5f7c33efaa27a
Badr Choubai <badrchoubai@outlook.com>
2020-05-14 22:53:42 -0600

    add starter code

---
+++ procedural/index.js
@@ -0,0 +1,11 @@
+/**
+ * Procedural Code
+ *
+ * Basics of our application:
+ * - We want to listen for a submission of our login form.
+ * - Get the values the user enters (username, password).
+ * - Validate the inputs and correctly handle errors.
+ * - At the end, create a new User object and log it to the console.
+ * */
+
+console.log('Procedural Code Loaded');
